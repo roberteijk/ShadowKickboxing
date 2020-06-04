ab3aa052e356663b84feb86c233415a4f65edcef
Robert van den Eijk <robert@vandeneijk.net>
2020-06-04 19:44:25 +0200

    Bug fix endless loop. Reverts previous update.

--- src/main/java/net/vandeneijk/shadowkickboxing/startup/SeedDatabase.java
+++ src/main/java/net/vandeneijk/shadowkickboxing/startup/SeedDatabase.java
@@ -161,15 +161,13 @@ public class SeedDatabase {
     }
 
     private void seedFight() {
-        for (int i = 1; i <= 7; i++) {
-            for (Speed speed : speedService.findAll()) {
-                for (Length length : lengthService.findAll()) {
-                    for (DefensiveMode defensiveMode : defensiveModeService.findAll()) {
-                        for (Expertise expertise : expertiseService.findAll()) {
-                            long numberOfFightsByCriteria = fightService.countBySpeedAndLengthAndDefensiveModeAndExpertiseAndZdtFirstDownload(speed, length, defensiveMode, expertise, null);
-                            for (long j = numberOfFightsByCriteria; j < i; j++) {
-                                fightFactory.createFight("English", speed, length, defensiveMode, expertise);
-                            }
+        for (Speed speed : speedService.findAll()) {
+            for (Length length : lengthService.findAll()) {
+                for (DefensiveMode defensiveMode : defensiveModeService.findAll()) {
+                    for (Expertise expertise : expertiseService.findAll()) {
+                        long numberOfFightsByCriteria = fightService.countBySpeedAndLengthAndDefensiveModeAndExpertiseAndZdtFirstDownload(speed, length, defensiveMode, expertise, null);
+                        for (long i = numberOfFightsByCriteria; i < 7; i++) {
+                            fightFactory.createFight("English", speed, length, defensiveMode, expertise);
                         }
                     }
                 }
